6b49a18190e3003086b4e1d6d052e130788a5cdd
EduPav <edumarpav@yahoo.com.ar>
2025-02-13 17:17:14 -0300

    feat: enable CORS

--- spoodler/bootstrap/api_init.php
+++ spoodler/bootstrap/api_init.php
@@ -16,3 +16,15 @@ $app->map('sendSuccess', function (array $data, int $statusCode = 200) {
 $app->map('sendError', function (string $message, int $statusCode) {
     Flight::jsonHalt(["data" => [], "message" => $message], $statusCode);
 });
+
+// CORS
+header("Access-Control-Allow-Origin: *"); // Allows any domain (you can specify a specific one)
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Allowed request types
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Allowed headers
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
